ccc8a427f80018bf46d3189ea4fbd5eb21b1d92a
Keith Owino <designsolutions1629@gmail.com>
2025-12-24 21:00:52 +0300

    Delete unwanted videos.

--- src/Constants/videos.js
+++ src/Constants/videos.js
@@ -1,7 +1,5 @@
 import heroClip1Main from '../assets/heroClip1Main.mp4';
-import heroClip2ferile from '../assets/heroClip2ferile.mp4';
-import heroClip1Trailer from '../assets/heroClip1Trailer.mp4';
 
 export default {
-    heroClip1Main, heroClip2ferile, heroClip1Trailer
+    heroClip1Main
 }
